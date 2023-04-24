8b7f05d801f4795a9f66c56efd801b9502dc89a3
Vitaliy <73063388+vitpoverenov@users.noreply.github.com>
2023-04-25 00:08:25 +0200

    Math programm using JS
    First update

---
+++ script.js
@@ -0,0 +1,19 @@
+let button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+  let result = 0;
+
+  let a1 = document.getElementById("q1").value;
+  alert(a1);
+}
+/**
+ Проверяет вызовом в браузере алертом работает ли кнопка
+
+ function checkTest() {
+  alert(2);
+}
+
+
+ */
