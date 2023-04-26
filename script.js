d51a9a17041f7c9ce3339856eadcf3b934ed3ea2
Vitalii Povierienov | Front-end developer <73063388+vitpoverenov@users.noreply.github.com>
2023-04-26 23:07:47 +0200

    Add 2 QA + Add button

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
-let button = document.getElementById("finish");
 
-button.addEventListener("click", checkTest);
-
-function checkTest() {
-  let result = 0;
-
-  let a1 = document.getElementById("q1").value;
-  alert(a1);
-}
 /**
- Проверяет вызовом в браузере алертом работает ли кнопка
+ * add event on element
+ */
 
- function checkTest() {
-  alert(2);
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
 }
 
 
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+
+/**
+ * close navbar when click on any navbar links
  */
+
+ const navLinks = document.querySelectorAll("[data-nav-link]");
+
+ const closeNavbar = function () {
+   navbar.classList.remove("active");
+   overlay.classList.remove("active");
+ }
+ 
+ addEventOnElem(navLinks, "click", closeNavbar);
+ 
+ 
+ 
+ /**
+  * header active when scroll down
+  */
+ 
+ const header = document.querySelector("[data-header]");
+ 
+ const headerActive = function () {
+   window.scrollY > 100 ? header.classList.add("active")
+     : header.classList.remove("active");
+ }
+ 
+ addEventOnElem(window, "scroll", headerActive);
+ 
+ 
+ 
+ /**
+  * accordion toggle
+  */
+ 
+ const accordionAction = document.querySelectorAll("[data-accordion-action]");
+ 
+ const toggleAccordion = function () { this.classList.toggle("active"); }
+ 
+ addEventOnElem(accordionAction, "click", toggleAccordion);
+
+
+
+/*const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("data-overlay")
+
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active")
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar); */
